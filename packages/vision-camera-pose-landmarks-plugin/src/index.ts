22f721d003ace953f1b70917be4d23f863b8e6cb
pchalupa <chalupa.petr93@gmail.com>
2025-11-11 22:52:18 +0100

    add types

--- packages/vision-camera-pose-landmarks-plugin/src/index.ts
+++ packages/vision-camera-pose-landmarks-plugin/src/index.ts
@@ -1,7 +1,44 @@
 import { type Frame, VisionCameraProxy } from 'react-native-vision-camera';
 
+export enum PoseLandmarkType {
+  NOSE,
+  LEFT_EYE_INNER,
+  LEFT_EYE,
+  LEFT_EYE_OUTER,
+  RIGHT_EYE_INNER,
+  RIGHT_EYE,
+  RIGHT_EYE_OUTER,
+  LEFT_EAR,
+  RIGHT_EAR,
+  LEFT_MOUTH,
+  RIGHT_MOUTH,
+  LEFT_SHOULDER,
+  RIGHT_SHOULDER,
+  LEFT_ELBOW,
+  RIGHT_ELBOW,
+  LEFT_WRIST,
+  RIGHT_WRIST,
+  LEFT_PINKY,
+  RIGHT_PINKY,
+  LEFT_INDEX,
+  RIGHT_INDEX,
+  LEFT_THUMB,
+  RIGHT_THUMB,
+  LEFT_HIP,
+  RIGHT_HIP,
+  LEFT_KNEE,
+  RIGHT_KNEE,
+  LEFT_ANKLE,
+  RIGHT_ANKLE,
+  LEFT_HEEL,
+  RIGHT_HEEL,
+  LEFT_FOOT_INDEX,
+  RIGHT_FOOT_INDEX,
+  UNKNOWN,
+}
+
 export type PoseLandmark = {
-  type: string;
+  type: PoseLandmarkType;
   x: number;
   y: number;
 };
