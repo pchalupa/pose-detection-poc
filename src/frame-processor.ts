79e8e017498bd9d0330d2bb432c1e79d454234d8
pchalupa <chalupa.petr93@gmail.com>
2025-11-12 10:04:00 +0100

    add drawing

--- src/frame-processor.ts
+++ src/frame-processor.ts
@@ -5,18 +5,85 @@ import {
   type Orientation,
 } from 'react-native-vision-camera';
 import { Worklets } from 'react-native-worklets-core';
-import { getPoseLandmarks } from 'vision-camera-pose-landmarks-plugin';
+import {
+  getPoseLandmarks,
+  PoseLandmarkType,
+  type PoseLandmark,
+} from 'vision-camera-pose-landmarks-plugin';
 
-function drawLandmarks(frame: DrawableFrame, landmarks: { type: string; x: number; y: number }[]) {
+const POSE_CONNECTIONS: [PoseLandmarkType, PoseLandmarkType][] = [
+  // Face
+  [PoseLandmarkType.LEFT_EYE_INNER, PoseLandmarkType.LEFT_EYE],
+  [PoseLandmarkType.LEFT_EYE, PoseLandmarkType.LEFT_EYE_OUTER],
+  [PoseLandmarkType.LEFT_EYE_OUTER, PoseLandmarkType.LEFT_EAR],
+  [PoseLandmarkType.RIGHT_EYE_INNER, PoseLandmarkType.RIGHT_EYE],
+  [PoseLandmarkType.RIGHT_EYE, PoseLandmarkType.RIGHT_EYE_OUTER],
+  [PoseLandmarkType.RIGHT_EYE_OUTER, PoseLandmarkType.RIGHT_EAR],
+  [PoseLandmarkType.NOSE, PoseLandmarkType.LEFT_EYE_INNER],
+  [PoseLandmarkType.NOSE, PoseLandmarkType.RIGHT_EYE_INNER],
+  [PoseLandmarkType.LEFT_MOUTH, PoseLandmarkType.RIGHT_MOUTH],
+
+  // Arms
+  [PoseLandmarkType.LEFT_SHOULDER, PoseLandmarkType.RIGHT_SHOULDER],
+  [PoseLandmarkType.LEFT_SHOULDER, PoseLandmarkType.LEFT_ELBOW],
+  [PoseLandmarkType.LEFT_ELBOW, PoseLandmarkType.LEFT_WRIST],
+  [PoseLandmarkType.LEFT_WRIST, PoseLandmarkType.LEFT_THUMB],
+  [PoseLandmarkType.LEFT_WRIST, PoseLandmarkType.LEFT_PINKY],
+  [PoseLandmarkType.LEFT_WRIST, PoseLandmarkType.LEFT_INDEX],
+  [PoseLandmarkType.LEFT_PINKY, PoseLandmarkType.LEFT_INDEX],
+  [PoseLandmarkType.RIGHT_SHOULDER, PoseLandmarkType.RIGHT_ELBOW],
+  [PoseLandmarkType.RIGHT_ELBOW, PoseLandmarkType.RIGHT_WRIST],
+  [PoseLandmarkType.RIGHT_WRIST, PoseLandmarkType.RIGHT_THUMB],
+  [PoseLandmarkType.RIGHT_WRIST, PoseLandmarkType.RIGHT_PINKY],
+  [PoseLandmarkType.RIGHT_WRIST, PoseLandmarkType.RIGHT_INDEX],
+  [PoseLandmarkType.RIGHT_PINKY, PoseLandmarkType.RIGHT_INDEX],
+
+  // Torso
+  [PoseLandmarkType.LEFT_SHOULDER, PoseLandmarkType.LEFT_HIP],
+  [PoseLandmarkType.RIGHT_SHOULDER, PoseLandmarkType.RIGHT_HIP],
+  [PoseLandmarkType.LEFT_HIP, PoseLandmarkType.RIGHT_HIP],
+
+  // Legs
+  [PoseLandmarkType.LEFT_HIP, PoseLandmarkType.LEFT_KNEE],
+  [PoseLandmarkType.LEFT_KNEE, PoseLandmarkType.LEFT_ANKLE],
+  [PoseLandmarkType.LEFT_ANKLE, PoseLandmarkType.LEFT_HEEL],
+  [PoseLandmarkType.LEFT_HEEL, PoseLandmarkType.LEFT_FOOT_INDEX],
+  [PoseLandmarkType.LEFT_ANKLE, PoseLandmarkType.LEFT_FOOT_INDEX],
+  [PoseLandmarkType.RIGHT_HIP, PoseLandmarkType.RIGHT_KNEE],
+  [PoseLandmarkType.RIGHT_KNEE, PoseLandmarkType.RIGHT_ANKLE],
+  [PoseLandmarkType.RIGHT_ANKLE, PoseLandmarkType.RIGHT_HEEL],
+  [PoseLandmarkType.RIGHT_HEEL, PoseLandmarkType.RIGHT_FOOT_INDEX],
+  [PoseLandmarkType.RIGHT_ANKLE, PoseLandmarkType.RIGHT_FOOT_INDEX],
+];
+
+function drawLandmarks(frame: DrawableFrame, landmarks: PoseLandmark[]) {
   'worklet';
 
-  for (const point of landmarks) {
-    const { x, y } = point;
+  const landmarkMap = new Map<PoseLandmarkType, PoseLandmark>();
+  const point = Skia.Paint();
+  const line = Skia.Paint();
 
-    const paint = Skia.Paint();
-    paint.setColor(Skia.Color('red'));
+  line.setColor(Skia.Color('lightblue'));
+  line.setStrokeWidth(2);
+  line.setStyle(1);
 
-    frame.drawCircle(x, y, 5, paint);
+  point.setColor(Skia.Color('blue'));
+  point.setStyle(1);
+
+  for (const landmark of landmarks) {
+    landmarkMap.set(landmark.type, landmark);
+  }
+
+  for (const [start, end] of POSE_CONNECTIONS) {
+    const startPoint = landmarkMap.get(start);
+    const endPoint = landmarkMap.get(end);
+
+    if (startPoint && endPoint)
+      frame.drawLine(startPoint.x, startPoint.y, endPoint.x, endPoint.y, line);
+  }
+
+  for (const landmark of landmarks) {
+    frame.drawCircle(landmark.x, landmark.y, 3, point);
   }
 }
 
@@ -28,7 +95,6 @@ const previewOrientation = Worklets.createSharedValue<Orientation>('portrait');
 export const frameProcessor = createSkiaFrameProcessor(
   (frame) => {
     'worklet';
-
     const landmarks = getPoseLandmarks(frame);
 
     frame.render();
